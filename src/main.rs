5a27613d9c001f2a47e64af3b497813e90bad75e
mswift42 <martin.haesler@gmail.com>
2018-02-03 10:17:08 +0000

    Add programme struct.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate select;
+
+type IplayerDocument = select::document::Document;
+
+pub struct Programme<'a> {
+    pub title: &'a str,
+    pub subtitle: Option<&'a str>,
+    pub synopsis: &'a str,
+    pub pid: &'a str,
+    pub thumbnail: &'a str,
+    pub url: &'a str,
+    pub index: usize,
+}
+
 fn main() {
     println!("Hello, world!");
 }
