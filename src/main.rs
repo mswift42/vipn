3c8abc838bc9b17a8f36734fd773e653f055d7a4
mswift42 <martin.haesler@gmail.com>
2018-07-09 06:38:49 +0100

    Add extern crate definitions.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 #![feature(custom_attribute)]
+extern crate reqwest;
+extern crate select;
+extern crate url;
+extern crate serde;
+extern crate serde_derive;
 
 mod tv;
 #[macro_use] extern crate failure;
